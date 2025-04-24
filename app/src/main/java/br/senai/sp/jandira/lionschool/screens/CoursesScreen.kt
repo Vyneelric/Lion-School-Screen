8f4e00caef3ed4643878e38490a22e48020f4e5e
VINÍCIUS JOSÉ LOPES CARDOSO <24122805@dsn127m1246733.cfp127aluno.local>
2025-04-24 17:23:15 -0300

    Primeira tela OK

--- app/src/main/java/br/senai/sp/jandira/lionschool/screens/CoursesScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/lionschool/screens/CoursesScreen.kt
@@ -1,2 +1,208 @@
 package br.senai.sp.jandira.lionschool.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import br.senai.sp.jandira.lionschool.R
+
+@Composable
+fun CoursesScreen(navegacao: NavHostController?){
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp, vertical = 20.dp)
+            .background(Color.White)
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxSize()
+                .background(Color.Transparent),
+            horizontalAlignment = Alignment.Start
+        ){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(90.dp)
+                    .padding(bottom = 5.dp)
+                    .background(color = Color.Transparent),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ){
+                Image(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .size(50.dp)
+                    ,
+                    painter = painterResource(R.drawable.logo),
+                    contentDescription = "",
+                )
+                Text(
+                    text = stringResource(R.string.name_school_text),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF3347B0),
+                    modifier = Modifier
+                        .padding(horizontal = 5.dp)
+                )
+            }
+
+            Card(
+                modifier = Modifier
+                    .height(1.dp)
+                    .fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFFFFC23D)
+                ),
+                shape = CircleShape
+
+            ){}
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(size = 8.dp),
+                    value = "",
+                    label = {
+                        Text(
+                            stringResource(
+                                R.string.label_text_field
+                            ),
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.W400,
+                            color = Color(0xFF9E9FA4)
+                        )
+                    },
+                    onValueChange = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.lupa),
+                            contentDescription = "",
+                            tint = Color.Gray
+                        )
+                    },
+                    colors = TextFieldDefaults.colors(
+                        unfocusedIndicatorColor = Color(0xFFFFC23D),
+                        unfocusedLabelColor = Color(0xFFFFC23D)
+                    )
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    painter = painterResource(R.drawable.lista),
+                    contentDescription = ""
+                )
+                Text(
+                    text = stringResource(R.string.title_courses),
+                    fontSize = 26.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF3347B0)
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .background(Color.Magenta)
+            ){
+                Column(
+                    modifier = Modifier
+                        .height(300.dp)
+                        .background(Color.Green)
+                ){
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp)
+                            .background(Color.Blue),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Image(
+                            modifier = Modifier
+                                .size(130.dp)
+                                .padding(horizontal = 10.dp)
+                            ,
+                            painter = painterResource(R.drawable.ds),
+                            contentDescription = "",
+                        )
+                        Text(
+                            text = stringResource(R.string.acronym_ds),
+                            fontSize = 64.sp,
+                            fontWeight = FontWeight.W800,
+                            color = Color(0xFFFFC23D)
+                        )
+                    }
+                    Column() {
+                        Text(
+                            text = stringResource(R.string.desc_course_ds),
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.W800,
+                            color = Color(0xFFFFC23D)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+
+private fun CoursesScreenPreview(){
+    CoursesScreen(navegacao = null)
+}
--- app/src/main/java/br/senai/sp/jandira/lionschool/screens/HomeLionSchool.kt
+++ app/src/main/java/br/senai/sp/jandira/lionschool/screens/HomeLionSchool.kt
@@ -1,7 +1,9 @@
 package br.senai.sp.jandira.lionschool.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,15 +13,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,24 +49,25 @@ fun HomeScreenLionSchool(navegacao: NavHostController?){
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.Black)
+                .background(Color.Transparent),
+            horizontalAlignment = Alignment.CenterHorizontally
         ){
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(280.dp)
                     .padding(top = 40.dp)
-                    .background(color = Color.Red),
+                    .background(color = Color.Transparent),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center
             ){
                 Image(
                     modifier = Modifier
                         .size(200.dp)
+                        .padding(horizontal = 15.dp)
                     ,
                     painter = painterResource(R.drawable.logo),
                     contentDescription = "",
-
                     )
                 Text(
                     text = stringResource(R.string.name_school),
@@ -64,7 +77,109 @@ fun HomeScreenLionSchool(navegacao: NavHostController?){
                 )
             }
 
+            Column(
+                modifier = Modifier
+                    .background(Color.Transparent)
+                    .height(350.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(
+                    text = stringResource(R.string.subtitle),
+                    fontSize = 26.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF3347B0),
+                    modifier = Modifier
+                        .padding(bottom = 10.dp, top = 90.dp)
+                )
 
+                Card(
+                    modifier = Modifier
+                        .height(6.dp)
+                        .width(70.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color(0xFFFFC23D)
+                    ),
+                    shape = CircleShape
+
+                ){}
+
+                Text(
+                    modifier = Modifier
+                        .padding(top = 30.dp)
+                        .padding(horizontal = 20.dp),
+                    text = stringResource(R.string.desc),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.W400,
+                    color = Color(0xFF9E9FA4),
+                    textAlign = TextAlign.Center
+                )
+
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Transparent),
+                horizontalArrangement = Arrangement.Center
+            ){
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .width(280.dp)
+                        .height(48.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFFFC23D)
+                    ),
+                    border = BorderStroke(
+                        width = 2.dp,
+                        color = Color(0xFF3347B0)
+                    )
+                ) {
+                    Text(
+                        text = stringResource(R.string.text_button),
+                        color = Color(0xFF3347B0),
+                        fontSize = 16.sp,
+                        )
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .width(320.dp)
+                    .background(Color.Transparent)
+                    .padding(top = 115.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ){
+                Image(
+                    modifier = Modifier
+                        .size(33.dp)
+                    ,
+                    painter = painterResource(R.drawable.youtube),
+                    contentDescription = "",
+                )
+                Image(
+                    modifier = Modifier
+                        .size(33.dp)
+                    ,
+                    painter = painterResource(R.drawable.twitter),
+                    contentDescription = "",
+                )
+                Image(
+                    modifier = Modifier
+                        .size(33.dp)
+                    ,
+                    painter = painterResource(R.drawable.instagram),
+                    contentDescription = "",
+                )
+                Image(
+                    modifier = Modifier
+                        .size(33.dp)
+                    ,
+                    painter = painterResource(R.drawable.facebook),
+                    contentDescription = "",
+                )
+            }
         }
     }
 }
